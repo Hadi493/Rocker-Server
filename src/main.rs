85a0e60539e8998c1ea8400b41b26c65f71b9101
Hadi493 <hadialam493@gmail.com>
2024-11-29 01:00:34 +0600

    simple

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,12 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::serde::json::Json;
-use serde::{Deserialize, Serialize};
-
-// Define data structures
-#[derive(Serialize, Deserialize)]
-struct Message {
-    content: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct User {
-    id: u32,
-    name: String,
-    email: String,
-}
-
-// Route handlers
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, welcome to Rocket API! it's my first Rocket server"
-}
-
-#[get("/user/<id>")]
-fn get_user(id: u32) -> Json<User> {
-    let user = User {
-        id,
-        name: String::from("John Doe"),
-        email: String::from("john@example.com"),
-    };
-    Json(user)
-}
-
-#[post("/message", data = "<message>")]
-fn create_message(message: Json<Message>) -> Json<Message> {
-    message
-}
-
-#[get("/hello?<name>")]
-fn hello(name: Option<String>) -> String {
-    match name {
-        Some(name) => format!("Hello, {}!", name),
-        None => String::from("Hello, world!"),
-    }
-}
-
-#[catch(404)]
-fn not_found() -> &'static str {
-    "Resource not found!"
+    "Hello, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![index, get_user, create_message, hello])
-        .register("/", catchers![not_found])
+    rocket::build().mount("/", routes![index])
 }
